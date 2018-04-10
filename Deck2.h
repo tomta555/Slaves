e3de005b9a24083980f8001b8813363eb8a003ae
Thanawong Saneewong Na Ayutthaya <tomta555@hotmail.com>
2018-04-10 21:56:34 +0700

    แก้ Deck2.h

--- Deck2.h
+++ Deck2.h
@@ -7,6 +7,7 @@
 using namespace std;
 
 class Deck{ 
+		
 	public:
 		vector<int> cards;
 		vector<string> cardFace;
@@ -14,67 +15,97 @@ class Deck{
 		Deck();
 		void ShuffleCard();
 		void ShuffleFace();
+//		vector<string> GetDeckFace();
 };
 Deck::Deck(){
 	string vectmp[52]={"3\5","3\4","3\3","3\6","4\5","4\4","4\3","4\6","5\5","5\4","5\3","5\6","6\5","6\4","6\3","6\6","7\5","7\4","7\3","7\6","8\5","8\4","8\3","8\6",
 	"9\5","9\4","9\3","9\6","10\5","10\4","10\3","10\6","J\5","J\4","J\3","J\6","Q\5","Q\4","Q\3","Q\6","K\5","K\4","K\3","K\6","A\5","A\4","A\3","A\6","2\5","2\4","2\3","2\6"};
+	
+	cardFace.assign(&vectmp[0], &vectmp[0]+52);
+	
 	for(int i=0;i<52;i++){
 		cards.push_back(i);
 	}
-	cardFace.assign(&vectmp[0], &vectmp[0]+52);
 }
 void Deck::ShuffleCard(){
-	vector<int> x;
 		random_shuffle(cards.begin(),cards.end());
 	}
 
 void Deck::ShuffleFace(){
-//	vector<string> faceShuff;
 	for(int i=0;i<52;i++){	
 		faceShuff.push_back(cardFace[cards[i]]);
 	}
-
 }
+//vector<string> Deck::GetDeckFace(){
+//	return cardFace;
+//}
 
 class Player{
 	string name;
 	string type;
-	int cardcount;
-	vector<int> cardOrder;
-	vector<int> allcards;
-	vector<string> allfaces;	
+	int cardcount,cardcountcst;
+	vector<string> allfaces;
+	vector<int> plcards;
+	vector<string> plfaces;
+	vector<int> sortedcards;
+	vector<string> sortedfaces;
 	public:
-		Player(string,vector<int>,vector<string>,int,int);
-		bool isKing();
+		
+		Player(string,vector<string>,vector<int>,vector<string>,int,int);
+		bool isWin();
 		bool isEmptyHand();
+		void sortcard();
+		void sortface();
 		vector<int> GetCard();
 		vector<string> GetFace();
+//		vector<int> GetCardd();
+//		vector<string> GetFaced();
 };
-Player::Player(string n,vector<int> c, vector<string> f,int first,int last){
+Player::Player(string n,vector<string> af ,vector<int> c, vector<string> f,int first,int last){
+	allfaces = af;
 	name = n;
+	type = "People";
+	cardcountcst=last-first;
+	cardcount=last-first;
 	for(int i=first;i<last;i++){
-//		cardOrder.push_back(i);
-		allcards.push_back(c[i]);
-		allfaces.push_back(f[i]);
+		plcards.push_back(c[i]);
+		plfaces.push_back(f[i]);
+	}
+	sortedcards=plcards;
+}
+void Player::sortcard(){
+	sort(sortedcards.begin(),sortedcards.end());
+}
+void Player::sortface(){
+	for(int i=0;i<cardcountcst;i++){
+		sortedfaces.push_back(allfaces[sortedcards[i]]);
 	}
 }
+//vector<int> Player::GetCardd(){
+//	vector<int> Pcards = plcards;
+//	return Pcards;
+//}
+//vector<string> Player::GetFaced(){
+//	vector<string> PcardFace = plfaces;
+//	return PcardFace;
+//}
 vector<int> Player::GetCard(){
-	vector<int> Pcards = allcards;
+	vector<int> Pcards = sortedcards;
 	return Pcards;
 }
 vector<string> Player::GetFace(){
-	vector<string> PcardFace = allfaces;
+	vector<string> PcardFace = sortedfaces;
 	return PcardFace;
 }
-//bool Player::isKing(){
-//	if(cardcount == 0){
-//		type = "King";
-//		return true;	
-//	}else return false;	
-//}
-//bool isEmptyHand(){
-//	if(cardcount == 0){
-//		type = "People";
-//		return true;	
-//	}else return false;	
-//}
+bool Player::isWin(){
+	if(cardcount == 0){
+		type = "King";
+		return true;	
+	}else return false;	
+}
+bool Player::isEmptyHand(){
+	if(cardcount == 0){
+		type = "People";
+		return true;	
+	}else return false;	
+}
--- main-Deck.cpp
+++ main-Deck.cpp
@@ -24,40 +24,87 @@ int main(){
 	Deck c1;
 	c1.ShuffleCard();
 	c1.ShuffleFace();
-//	if(num==2){
-		Player p1(name[0],c1.cards,c1.faceShuff,0,26);
-		Player p2(name[1],c1.cards,c1.faceShuff,26,52);	
-//	
-//	}else if(num==3){ 
-//		Player p1(name[0],c1.cards,c1.faceShuff,0,17);
-//		Player p2(name[1],c1.cards,c1.faceShuff,17,34);
-//		Player p3(name[2],c1.cards,c1.faceShuff,34,52); //  1 more card than other
-//	}else if(num==4){
-//		Player p1(name[0],c1.cards,c1.faceShuff,0,13);
-//		Player p2(name[1],c1.cards,c1.faceShuff,13,26);
-//		Player p3(name[2],c1.cards,c1.faceShuff,26,39);
-//		Player p4(name[3],c1.cards,c1.faceShuff,39,52);
-//	}
-	
-//	vector<int> cax=p1.GetCard();
+
+	if(num==2){
+		Player p1(name[0],c1.cardFace,c1.cards,c1.faceShuff,0,26);
+		Player p2(name[1],c1.cardFace,c1.cards,c1.faceShuff,26,52);
+			p1.sortcard();
+			p1.sortface();
+			p2.sortcard();
+			p2.sortface();
+	}else if(num==3){ 
+		Player p1(name[0],c1.cardFace,c1.cards,c1.faceShuff,0,17);
+		Player p2(name[1],c1.cardFace,c1.cards,c1.faceShuff,17,34);
+		Player p3(name[2],c1.cardFace,c1.cards,c1.faceShuff,34,52); //  1 more card than other
+			p1.sortcard();
+			p1.sortface();
+			p2.sortcard();
+			p2.sortface();
+			p3.sortcard();
+			p3.sortface();
+	}else if(num==4){
+		Player p1(name[0],c1.cardFace,c1.cards,c1.faceShuff,0,13);
+		Player p2(name[1],c1.cardFace,c1.cards,c1.faceShuff,13,26);
+		Player p3(name[2],c1.cardFace,c1.cards,c1.faceShuff,26,39);
+		Player p4(name[3],c1.cardFace,c1.cards,c1.faceShuff,39,52);
+			p1.sortcard();
+			p1.sortface();
+			p2.sortcard();
+			p2.sortface();
+			p3.sortcard();
+			p3.sortface();
+			p4.sortcard();
+			p4.sortface();
+	}
+
+
+
+//      print    shuffle      cards
+//	vector<int> caxx=p1.GetCardd();
 //	for(int i=0;i<26;i++){
+//		cout << caxx[i] << " ";
+//	}
+//	cout << endl;
+//	vector<string> caff=p1.GetFaced();
+//	for(int i=0;i<26;i++){
+//		cout << caff[i] << " ";
+//	}
+//	cout << endl;
+	
+//      print     sort       cards
+
+//	vector<int> cax=p1.GetCard();
+//	for(int i=0;i<17;i++){
 //		cout << cax[i] << " ";
 //	}
 //	cout << endl;
 //	vector<string> caf=p1.GetFace();
-//	for(int i=0;i<26;i++){
+//	for(int i=0;i<17;i++){
 //		cout << caf[i] << " ";
 //	}
 //	cout << endl;
-//		vector<int> cax2=p2.GetCard();
-//	for(int i=0;i<26;i++){
-//		cout << cax2[i] << " ";
+//	vector<int> caxx=p2.GetCard();
+//	for(int i=0;i<17;i++){
+//		cout << caxx[i] << " ";
 //	}
 //	cout << endl;
-//	vector<string> caf2=p2.GetFace();
-//	for(int i=0;i<26;i++){
-//		cout << caf2[i] << " ";
+//	vector<string> caff=p2.GetFace();
+//	for(int i=0;i<17;i++){
+//		cout << caff[i] << " ";
 //	}
+//	cout << endl;
+//	vector<int> caxxx=p3.GetCard();
+//	for(int i=0;i<18;i++){
+//		cout << caxxx[i] << " ";
+//	}
+//	cout << endl;
+//	vector<string> cafff=p3.GetFace();
+//	for(int i=0;i<18;i++){
+//		cout << cafff[i] << " ";
+//	}
+//	cout << endl;
+
+
 
 	
 	
